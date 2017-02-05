1b33582efd33b34e203d7fb049ec14ac898e760e
cocofluo <kcoyne17@wooster.edu>
2017-02-05 12:44:42 -0500

    Update 1
    Added rounding function.

--- CFU_Calculator.py
+++ CFU_Calculator.py
@@ -66,10 +66,11 @@ def main():
     print ''
     print "Each dilution's CFU/ml are: ", CFU_dictionary
     CFU_Average = Average_Plates(CFU_dictionary)
+    Rounded_CFU_Average = round(CFU_Average, -((len(str(CFU_Average)))-4))
+      #Here, I rounded the average to 2 significant figures, using index 2 less than the length of my string.
     print ''
-    print 'Averaged CFU/ml is: ', CFU_Average 
+    print 'Averaged CFU/ml is: ', Rounded_CFU_Average 
     return  
-
 
 main()
     
